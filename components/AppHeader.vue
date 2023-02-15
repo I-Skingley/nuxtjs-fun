9a573749122d81058538bb899d3646792c40cfb6
I-Skingley <ijskingley@gmail.com>
2023-02-15 13:56:18 +0100

    Sidebar button functionality

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="header">
-    <button class="menu-button">
+    <button class="menu-button" @click="show = !show">
       <svg
         fill="var(--button-color)"
         class="hamburger"
         viewBox="0 0 100 100"
         width="40"
       >
-          <rect
-            class="line"
-            width="80"
-            height="10"
-            x="10"
-            y="15"
-            rx="5"
-          ></rect>
-          <rect
-            class="line"
-            width="80"
-            height="10"
-            x="10"
-            y="45"
-            rx="5"
-          ></rect>
-          <rect
-            class="line"
-            width="80"
-            height="10"
-            x="10"
-            y="75"
-            rx="5"
-          ></rect>
+        <rect
+          v-if="!show"
+          class="line"
+          width="80"
+          height="10"
+          x="10"
+          y="15"
+          rx="5"
+        ></rect>
+        <rect
+          v-if="!show"
+          class="line"
+          width="80"
+          height="10"
+          x="10"
+          y="45"
+          rx="5"
+        ></rect>
+        <rect
+          v-if="!show"
+          class="line"
+          width="80"
+          height="10"
+          x="10"
+          y="75"
+          rx="5"
+        ></rect>
       </svg>
     </button>
     <div class="header-items">
@@ -40,10 +43,20 @@
       <NuxtLink to="/" class="inline link header-item">Contact</NuxtLink>
     </div>
   </div>
-  <div class="menu">
+  <div class="menu" v-if="show">
     <NuxtLink to="/" class="menu-item">Home</NuxtLink>
     <NuxtLink to="/" class="menu-item">Overview</NuxtLink>
     <NuxtLink to="/" class="menu-item">About</NuxtLink>
     <NuxtLink to="/" class="menu-item">Contact</NuxtLink>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
